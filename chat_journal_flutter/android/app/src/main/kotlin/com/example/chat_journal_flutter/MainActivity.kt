96cc31dbb0628d33e6636e90a0fb18a4c87250d0
antikd8 <antikd8888@gmail.com>
2021-12-15 01:58:31 +0300

    Deleted old folder

--- chat_journal_flutter/android/app/src/main/kotlin/com/example/chat_journal_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chat_journal_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- chat_journal_flutter/lib/models/event.dart
+++
@@ -1,8 +0,0 @@
-class Event {
-  late String content;
-  late DateTime date;
-  bool isFavourite = false;
-  bool isSelected = false;
-  bool isEditing = false;
-  bool isEdited = false;
-}
