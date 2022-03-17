ebbaee5968cc541798915a9c69a3e7e57fe3e47a
Lizaveta <lizazaytseva16@gmail.com>
2022-03-17 13:50:22 +0300

    Lesson 7

--- 658710673/lib/utils/constants.dart
+++ 658710673/lib/utils/constants.dart
@@ -4,8 +4,11 @@ class Constants {
   static const mediumTextSize = 20.0;
   static const bodyTextSize = 16.0;
 
-  static const primaryColor = Color(0xFFedf0f7);
-  static const secondaryColor = Color(0xFF747DF2);
+  static const secondaryColor = Color(0xFFedf0f7);
+  static const primaryColor = Color(0xFF747DF2);
+  static const messageColor = Color(0xAA747DF2);
+  static const dateMessageColor = Color(0xFFEAB8C3);
+  static const selectedMessageColor = Color(0xFF747DF2);
   static const listShadowColor = Color(0x808541F6);
   static const iconColor = Colors.black;
 }
